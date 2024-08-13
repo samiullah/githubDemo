7282379f397578270d9fa580fe342d7c56010fa8
sami <mypc@Mys-MacBook-Pro.local>
2024-08-13 06:53:48 +0530

    Adding more files

--- Readme.md
+++ Readme.md
@@ -2,3 +2,19 @@ github demo
 
 // git add filename
 // git commit -m "Adding readme file"
+
+
+// Main
+
+
+// create a branch locally
+// do ur work there
+// add commit push to ur branch
+// on github u have to raise pr against main branch
+
+// then ur team lead will review ur changes and accept or dis approve it
+// after accepting ur code will be merged with develop
+
+
+/// Second developer comes
+//// git pull
---
+++ tests/example.spec.ts
@@ -0,0 +1,35 @@
+// import { test, expect } from '@playwright/test';
+
+// test('test', async ({ page }) => {
+//   test.slow()
+//   await page.goto('https://cypress-training.iliad-solutions.com/reg/');
+//   await page.getByRole('textbox', { name: 'Username:' }).click();
+//   await page.getByRole('textbox', { name: 'Username:' }).fill('admin');
+//   await page.getByLabel('Password:', { exact: true }).click();
+//   await page.getByLabel('Password:', { exact: true }).fill('changeme');
+//   await page.getByRole('button', { name: 'Login' }).click();
+//   await page.getByText('MASTERCARD_R').click();
+//   await page.getByRole('button', { name: 'OK' }).click();
+//   await page.getByRole('button', { name: 'Create Role' }).click();
+//   await page.getByLabel('Role Name:*:').fill('test');
+//   await page.getByLabel('Description:').click();
+//   await page.getByLabel('Description:').fill('test des');
+//   await page.getByRole('button', { name: 'OK' }).click();
+//   await page.getByText('test', { exact: true }).click();
+//   await page.getByText('test des').click();
+// });
+
+// repository
+
+// bit bucket
+// github
+
+
+// pull
+// push
+
+
+// for making any codebase as git repo , u have to intilaise it
+// comamnd is git init
+
+
